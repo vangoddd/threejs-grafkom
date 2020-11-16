e1197e314d8d922612c29a33b6d8d34a4a1108e7
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 12:10:45 +0700

    cleaning up

--- anim.js
+++ anim.js
@@ -1,71 +1,71 @@
 
 const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-            //menambahkan plane
-            var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 100);
-            var planeMats = new THREE.MeshPhongMaterial({
-                color: 0xffffff,
-                roughness: 0.5,
-                side: THREE.DoubleSide
-            });
-            
-            var plane = new THREE.Mesh(planeGeometry, planeMats);
-            plane.position.y = -50;
-            plane.rotation.x =  90 * (Math.PI/180);
-            scene.add(plane);
+//menambahkan plane
+var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 100);
+var planeMats = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    roughness: 0.5,
+    side: THREE.DoubleSide
+});
 
-            //menambahkan cube
-            var geometry;
-            var cube;
-            var material;
+var plane = new THREE.Mesh(planeGeometry, planeMats);
+plane.position.y = -50;
+plane.rotation.x =  90 * (Math.PI/180);
+scene.add(plane);
 
-            var colors = [0x00ff9f, 0x00b8ff, 0x001eff, 0xbd00ff];
+//menambahkan cube
+var geometry;
+var cube;
+var material;
 
-            geometry = new THREE.BoxGeometry(10, 10, 10);
-            material = new THREE.MeshLambertMaterial( { 
-                color: 0x00ff00, 
-                roughness: 1,
-            } );
-            material.wireframe = true;
-            cube = new THREE.Mesh(geometry, material);
+var colors = [0x00ff9f, 0x00b8ff, 0x001eff, 0xbd00ff];
 
-            cube.position.y = 15;
-            
-            scene.add(cube);
+geometry = new THREE.BoxGeometry(10, 10, 10);
+material = new THREE.MeshLambertMaterial( { 
+    color: 0x00ff00, 
+    roughness: 1,
+} );
+material.wireframe = true;
+cube = new THREE.Mesh(geometry, material);
 
-            //menambahkan lightsource
-            var light = new THREE.AmbientLight( 0xffffff, 0.2 );
-            
-            var pointLight = new THREE.PointLight( 0xffffff, 1, 400 );
-            pointLight.position.set(0, 25, 0);
+cube.position.y = 15;
 
-            scene.add(light);
-            scene.add(pointLight);
+scene.add(cube);
 
-            //mengubah posisi kamera awal
-            camera.position.z = 50;
-            camera.position.y = 25;
-            
-            //variable pendukung untuk memutar kamera
-            var count = 0;
-			const animate = function () {
-                count++;
-                requestAnimationFrame( animate );
-                
-                cube.rotation.x += 0.01;
-                cube.rotation.y += 0.02;
-                cube.rotation.z += 0.01;
+//menambahkan lightsource
+var light = new THREE.AmbientLight( 0xffffff, 0.2 );
 
-                camera.position.x = Math.cos(count * 0.002)*50;
-                camera.position.z = Math.sin(count * 0.002)*50;
-                camera.lookAt(0,20,0);
+var pointLight = new THREE.PointLight( 0xffffff, 1, 400 );
+pointLight.position.set(0, 25, 0);
 
-				renderer.render( scene, camera );
-			};
+scene.add(light);
+scene.add(pointLight);
 
-			animate();
+//mengubah posisi kamera awal
+camera.position.z = 50;
+camera.position.y = 25;
+
+//variable pendukung untuk memutar kamera
+var count = 0;
+const animate = function () {
+    count++;
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.02;
+    cube.rotation.z += 0.01;
+
+    camera.position.x = Math.cos(count * 0.002)*50;
+    camera.position.z = Math.sin(count * 0.002)*50;
+    camera.lookAt(0,20,0);
+
+    renderer.render( scene, camera );
+};
+
+animate();
--- shadow.js
+++ shadow.js
@@ -1,96 +1,96 @@
 
 const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
-            renderer.shadowMap.enabled = true;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+renderer.shadowMap.enabled = true;
 
-            //menambahkan plane
-            var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 100);
-            var planeMats = new THREE.MeshPhongMaterial({
-                color: 0xffffff,
-                roughness: 0.5,
-                side: THREE.DoubleSide
-            });
-            
-            var plane = new THREE.Mesh(planeGeometry, planeMats);
-            plane.position.y = -50;
-            plane.rotation.x =  90 * (Math.PI/180);
-            plane.receiveShadow = true;
-            scene.add(plane);
+//menambahkan plane
+var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 100);
+var planeMats = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    roughness: 0.5,
+    side: THREE.DoubleSide
+});
 
-            //menambahkan cube
-            var geometry;
-            var cube;
-            var material;
+var plane = new THREE.Mesh(planeGeometry, planeMats);
+plane.position.y = -50;
+plane.rotation.x =  90 * (Math.PI/180);
+plane.receiveShadow = true;
+scene.add(plane);
 
-            var colors = [0x00ff9f, 0x00b8ff, 0x001eff, 0xbd00ff];
+//menambahkan cube
+var geometry;
+var cube;
+var material;
 
-            var objectAmount = 32;
-			for(i = 0; i < objectAmount; i++){
-                //menambahkan box
-                if(i < objectAmount / 2 ){
-                    geometry = new THREE.BoxGeometry(
-                        Math.random()*5 + 2, 
-                        Math.random()*5 + 2, 
-                        Math.random()*5 + 2);
-                }else{
-                    var segment = 16;
-                    var radius = Math.random()*5 + 1;
-                    geometry = new THREE.SphereGeometry(
-                        radius, segment, segment);
-                }
-                    
-                material = new THREE.MeshLambertMaterial( { 
-                    color: 0x00ff00, 
-                    roughness: 1,
-                } );
+var colors = [0x00ff9f, 0x00b8ff, 0x001eff, 0xbd00ff];
 
-                object = new THREE.Mesh( geometry, material );
+var objectAmount = 32;
+for(i = 0; i < objectAmount; i++){
+    //menambahkan box
+    if(i < objectAmount / 2 ){
+        geometry = new THREE.BoxGeometry(
+            Math.random()*5 + 2, 
+            Math.random()*5 + 2, 
+            Math.random()*5 + 2);
+    }else{
+        var segment = 16;
+        var radius = Math.random()*5 + 1;
+        geometry = new THREE.SphereGeometry(
+            radius, segment, segment);
+    }
+        
+    material = new THREE.MeshLambertMaterial( { 
+        color: 0x00ff00, 
+        roughness: 1,
+    } );
 
-                var tempColor = (colors[Math.floor(Math.random()*(colors.length))]);
-                object.material.color.set(tempColor);
-                
-                object.castShadow = true;
-                
-                object.position.x = Math.random()*100 - 50;
-                object.position.z = Math.random()*100 - 50;
-                object.position.y = Math.random()*50;
+    object = new THREE.Mesh( geometry, material );
 
-                scene.add(object);
-            }
-            
-            //menambahkan lightsource
-            var light = new THREE.AmbientLight( 0xffffff, 0.2 );
-            
-            var pointLight = new THREE.PointLight( 0xffffff, 1, 400 );
-            pointLight.position.set(0, 75, 0);
-            pointLight.castShadow = true;
+    var tempColor = (colors[Math.floor(Math.random()*(colors.length))]);
+    object.material.color.set(tempColor);
+    
+    object.castShadow = true;
+    
+    object.position.x = Math.random()*100 - 50;
+    object.position.z = Math.random()*100 - 50;
+    object.position.y = Math.random()*50;
 
-            scene.add(light);
-            scene.add(pointLight);
+    scene.add(object);
+}
+
+//menambahkan lightsource
+var light = new THREE.AmbientLight( 0xffffff, 0.2 );
+
+var pointLight = new THREE.PointLight( 0xffffff, 1, 400 );
+pointLight.position.set(0, 75, 0);
+pointLight.castShadow = true;
+
+scene.add(light);
+scene.add(pointLight);
 
 
-            //mengubah posisi kamera awal
-            camera.position.z = 50;
-            camera.position.y = 25;
+//mengubah posisi kamera awal
+camera.position.z = 50;
+camera.position.y = 25;
 
-            
-            //variable pendukung untuk memutar kamera
-            var count = 0;
-			const animate = function () {
-                count++;
-				requestAnimationFrame( animate );
 
-                camera.position.x = Math.cos(count * 0.002)*50;
-                camera.position.z = Math.sin(count * 0.002)*50;
-                camera.lookAt(0,20,0);
+//variable pendukung untuk memutar kamera
+var count = 0;
+const animate = function () {
+    count++;
+    requestAnimationFrame( animate );
 
-                camera.focus = (Math.sin(count * 0.002) * 5) +5;
+    camera.position.x = Math.cos(count * 0.002)*50;
+    camera.position.z = Math.sin(count * 0.002)*50;
+    camera.lookAt(0,20,0);
 
-				renderer.render( scene, camera );
-			};
+    camera.focus = (Math.sin(count * 0.002) * 5) +5;
 
-			animate();
+    renderer.render( scene, camera );
+};
+
+animate();
